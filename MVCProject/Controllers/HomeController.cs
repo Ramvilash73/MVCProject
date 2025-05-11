3a8818038c2f55a4d6a1fa38ac76325303780a24
AMD <akritigupta.edoro@gmail.com>
2025-05-10 20:16:32 -0400

    commit

--- MVCProject/Controllers/HomeController.cs
+++ MVCProject/Controllers/HomeController.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MVCProject.Models;
 namespace MVCProject.Controllers
 {
     public class HomeController : Controller
     {
+        [Route("")]
         public ActionResult Index()
         {
             return View();
         }
-
+        [Route("about")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -26,12 +27,30 @@ namespace MVCProject.Controllers
 
             return View();
         }
+        
         public ActionResult Registration()
         {
+            //RegistrationDll regDll = new RegistrationDll();
+            //BLRegistration.ConnectionDB(regDll);
             ViewBag.Message = "Your contact page.";
-
             return View();
-        } 
+        }
+
+        [HttpPost]
+        public ActionResult Registration(String name, String Email,String Phone,String DOB,String Aadhar,String Address,String Password)
+        {
+            RegistrationDll regDll = new RegistrationDll();
+            regDll.Name = name;
+            regDll.Email = Email;
+            regDll.Phone = Phone;
+            regDll.Dob = DOB;
+            regDll.AAdhar = Aadhar;
+            regDll.Address = Address;
+            regDll.password = Password;
+            BLRegistration.ConnectionDB(regDll);
+            return View();
+        }
+
         public ActionResult LoginFrom()
         {
             ViewBag.Message = "Your contact page.";
---
+++ MVCProject/Models/RegistrationDll.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MVCProject.Models
+{
+    public class RegistrationDll
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Dob { get; set; }
+        public string AAdhar { get; set; }
+        public string Address { get; set; }
+        public string password { get; set; }
+
+
+
+    }
+
+    public class BLRegistration
+    {
+        public static void ConnectionDB(RegistrationDll Robj)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("data source=DESKTOP-34SUGAB; database=Demo_DB; integrated security=true");
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = "INSERT INTO student (studentName, email,phone,dOB,addhar,address,password) VALUES ('" + Robj.Name + "','" + Robj.Email + "','" + Robj.Phone + "','" + Robj.Dob + "','" + Robj.AAdhar + "','" + Robj.Address + "','" + Robj.password + "')";
+                cmd.Connection = con;
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+        }
+    }
+}
